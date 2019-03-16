e42849253b9a45f65e1be9bc7e8d416db03958b4
Francisco Sandoval Cordero <fsandovalc@ucenfotec.ac.cr>
2019-03-15 21:28:32 -0600

    updates

---
+++ proyecto/prueba/public/js/controlador_citas.js
@@ -0,0 +1,7 @@
+$( "#datepicker" ).datepicker();
+
+$(function () {
+    $("#datepicker-13").datepicker({ maxDate: '+20', minDate: '0', beforeShowDay: $.datepicker.noWeekends });
+    $("#datepicker-13").datepicker("show");
+
+});
