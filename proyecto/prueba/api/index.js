c4a833c84f4eb9006ae1c3031e2b374b699ed724
Francisco Sandoval Cordero <fsandovalc@ucenfotec.ac.cr>
2019-03-17 11:24:56 -0600

    Update mongo atlas connect string
    Update connection string to Mongo Atlas with Caballeros de la mesa redonda.

--- proyecto/prueba/api/index.js
+++ proyecto/prueba/api/index.js
@@ -14,7 +14,10 @@ const express = require('express'),
  * Se definen las variables necesarias para la conexión con MongoDB
  */
 let db = mongoose.connection,
-    dburl = 'mongodb://admin_migue:clave@cluster0-shard-00-00-oiy6d.mongodb.net:27017,cluster0-shard-00-01-oiy6d.mongodb.net:27017,cluster0-shard-00-02-oiy6d.mongodb.net:27017/centro_educativo?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true', //usando mongoDb Atlas,
+
+    // Base de datos de loscaballerosdelamesaredonnda@gmail.com
+    dburl = 'mongodb://admin:admin1234@cluster0-shard-00-00-zmmky.gcp.mongodb.net:27017,cluster0-shard-00-01-zmmky.gcp.mongodb.net:27017,cluster0-shard-00-02-zmmky.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
+    //dburl = 'mongodb://admin_migue:clave@cluster0-shard-00-00-oiy6d.mongodb.net:27017,cluster0-shard-00-01-oiy6d.mongodb.net:27017,cluster0-shard-00-02-oiy6d.mongodb.net:27017/centro_educativo?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true', //usando mongoDb Atlas,
     //dburl = 'mongodb://pabs:1biblioteca9@ds163680.mlab.com:63680/bd_biblioteca' //usando mlab,
     port = 4000;
 
