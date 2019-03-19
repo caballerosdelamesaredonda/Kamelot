5bfb11eedc1eef714ef79168cb198c637fe52627
Francisco Sandoval Cordero <fsandovalc@ucenfotec.ac.cr>
2019-03-18 21:07:03 -0600

    Updates citas

--- proyecto/prueba/public/js/controlador_listar_citas.js
+++ proyecto/prueba/public/js/controlador_listar_citas.js
@@ -1,56 +1,73 @@
 'use strict';
 
-const tabla = document.querySelector('.tabla_info tbody');
+const inputFiltro = document.querySelector('#txtFiltro');
 
-let listar_citas = () => {
+//limpiarForm();
 
-    let listar_citas = [];
+let citas = consultar_citas();
 
-    let request = $.ajax({
-        url: "http://localhost:4000/api/consultar_citas",
-        method: "GET",
-        data: {
-        },
-        dataType: "json",
-        contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
-        async: false
-    });
+let mostrar_datos = () => {
 
-    request.done(function (res) {
-        listar_citas = res.citas;
-    });
+    let tabla = document.querySelector('.tabla_info tbody');
 
-    request.fail(function (jqXHR, textStatus) {
+    tabla.innerHTML = '';
 
-    });
-    return listar_citas;
+    for (let i = 0; i < citas.length; i++) {
+            let newdate = new Date(citas[i]['fecha']);
+            let month = newdate.getMonth() + 1;
+            let day = newdate.getDate();
+            let year = newdate.getFullYear();
+            let dateformated = month + '-' + day + '-' + year;
+            //console.log(month+'-'+day+'-'+year);
+
+            let fila = tabla.insertRow(); // Crea tr de la tabla
+            fila.insertCell().innerHTML = citas[i]['codigo'];
+            fila.insertCell().innerHTML = citas[i]['nombre'];
+            fila.insertCell().innerHTML = citas[i]['correo'];
+            //fila.insertCell().innerHTML = citas[i]['fecha'];
+            fila.insertCell().innerHTML = dateformated;
+    }
 
 };
 
-let mostrar_datos = () => {
-    let citas = listar_citas();
+let mostrar_datos_filtrados = () => {
+
+    let tabla = document.querySelector('.tabla_info tbody');
+    let filtro = inputFiltro.value;
+
+    tabla.innerHTML = '';
+
     for (let i = 0; i < citas.length; i++) {
+        if (
+        (citas[i]['codigo'].toLowerCase().includes(filtro.toLowerCase()))
+        || (citas[i]['nombre'].toLowerCase().includes(filtro.toLowerCase()))
+        || (citas[i]['correo'].toLowerCase().includes(filtro.toLowerCase()))
+        ) {
+            let newdate = new Date(citas[i]['fecha']);
+            let month = newdate.getMonth() + 1;
+            let day = newdate.getDate();
+            let year = newdate.getFullYear();
+            let dateformated = month + '-' + day + '-' + year;
+            //console.log(month+'-'+day+'-'+year);
 
-        let newdate = new Date(citas[i]['fecha']);
-        let month = newdate.getMonth() + 1;
-        let day = newdate.getDate();
-        let year = newdate.getFullYear();
-        let dateformated = month + '-' + day + '-' + year;
-        //console.log(month+'-'+day+'-'+year);
-
-        let fila = tabla.insertRow(); // Crea tr de la tabla
-        fila.insertCell().innerHTML = citas[i]['codigo'];
-        fila.insertCell().innerHTML = citas[i]['nombre'];
-        fila.insertCell().innerHTML = citas[i]['correo'];
-        //fila.insertCell().innerHTML = citas[i]['fecha'];
-        fila.insertCell().innerHTML = dateformated;
+            let fila = tabla.insertRow(); // Crea tr de la tabla
+            fila.insertCell().innerHTML = citas[i]['codigo'];
+            fila.insertCell().innerHTML = citas[i]['nombre'];
+            fila.insertCell().innerHTML = citas[i]['correo'];
+            //fila.insertCell().innerHTML = citas[i]['fecha'];
+            fila.insertCell().innerHTML = dateformated;
+        }
     }
+
 };
 
 mostrar_datos();
+mostrar_datos_filtrados();
+inputFiltro.addEventListener('keyup', mostrar_datos_filtrados);
+
 
 let citas_programadas = () => {
-    let citas = listar_citas();
+    let citas = consultar_citas();
     let listacitas = [];
     for (let i = 0; i < citas.length; i++) {
         let newdate = new Date(citas[i]['fecha']);
@@ -66,6 +83,53 @@ let citas_programadas = () => {
     return listacitas;
 };
 
+/* Días a deshabilitar en el calendario de citas mediante un arreglo */
+//var disableddates = ["3-20-2019", "3-21-2019"];
+var disableddates = citas_programadas();
+
+
+function DisableSpecificDates(date) {
+
+    var m = date.getMonth();
+    var d = date.getDate();
+    var y = date.getFullYear();
+
+    // First convert the date in to the mm-dd-yyyy format 
+    // Take note that we will increment the month count by 1 
+    var currentdate = (m + 1) + '-' + d + '-' + y;
+
+    // We will now check if the date belongs to disableddates array 
+    for (var i = 0; i < disableddates.length; i++) {
+
+        // Now check if the current date is in disabled dates array. 
+        if ($.inArray(currentdate, disableddates) != -1) {
+            return [false, 'highlight_citas'];
+        }
+    }
+
+    // In case the date is not present in disabled array, we will now check if it is a weekend. 
+    // We will use the noWeekends function
+    var weekenddate = $.datepicker.noWeekends(date);
+    return weekenddate;
+
+}
+
+// Function to show datepicker
+
+$(function () {
+    $("#datepicker").datepicker({
+        beforeShowDay: DisableSpecificDates
+    });
+});
+
+// Function to show limited datepicker dates
+
+$("#datepicker").datepicker();
+
+$(function () {
+    $("#datepicker-13").datepicker({ dateFormat: 'mm-dd-yy', maxDate: '+20', minDate: '0', beforeShowDay: DisableSpecificDates /* $.datepicker.noWeekends */ });
+    $("#datepicker-13").datepicker("show");
+});
 
 let limpiarForm = () => {
     document.forms['frm_citas'].reset();
