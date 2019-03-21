e5d48ce46aa356735502bdff45394af7797a4d10
Miguel Gutierrez <mgutierrezb@ucenfotec.ac.cr>
2019-03-20 23:55:13 -0600

    Final API USUARIOS

--- proyecto/prueba/api/componentes/usuarios/usuarios.model.js
+++ proyecto/prueba/api/componentes/usuarios/usuarios.model.js
@@ -1,38 +1,48 @@
 'use strict';
 const mongoose = require ('mongoose');
 
-let schema_centroe = new mongoose.Schema(
-    {
-        nombre : {type : String, required : true},
-        alias : {type : String, required : true},
-        cedula_juridica : {type : String, unique : true, required : true},
-        clave: {type : String, required: true},
-        tipo_centro : {type : String, required : true},
-        nivel_centro : {type : String, required : true},
-        foto : {type : String, required : false},
-        nombre_comercial : {type : String, required : true},
-        provincia : {type : String, required : true},
-        canton : {type : String, required : true},
+let schema_usuarios = new mongoose.Schema(
+    {   
+        //Compartido y padre de familia
+        nombre : {type : String, required : false},
+        segundo_nombre: {type : String, required : false},
+        papellido : {type : String, required : false},
+        sapellido : {type : String, required : false},
+        identificacion : {type : String, required : false},
+        cantidad_hijos : {type : String, required : false},
+        correo_electronico : {type : String, required : false},
+        clave: {type : String, required: false},
+        telefono : {type : String, required : false},
+        provincia : {type : String, required : false},
+        canton : {type : String, required : false},
         distrito: {type : String, required : false},
-        direccion: {type : String, required : true},
-        fecha_fundacion : {type : String, required : true},
-        referencia_historia : {type : String, required : true},
-        adjuntar_documentos : {type : String, required : true},
-        telefono : {type : String, required : true},
-        fax : {type : String, required : true},
-        sitio_web : {type : String, required : true},
-        cuentas_redesso : {type : String, required : true},
-        correo_electronico : {type : String, required : true},
-        contacto_nombre : {type : String, required : true},
-        contacto_papellido : {type : String, required : true},
-        contacto_sapellido : {type : String, required : true},
-        identificacion : {type : String, required : true},
-        departamento : {type : String, required : true}, 
-        telefono_contacto : {type : String, required : true},
+        direccion: {type : String, required : false},
+
+
+        //Exclusivo centro educativo//
+        alias : {type : String, required : false},
+        cedula_juridica : {type : String, unique : false, required : false},
+        tipo_centro : {type : String, required : false},
+        nivel_centro : {type : String, required : false},
+        foto : {type : String, required : false},
+        nombre_comercial : {type : String, required : false},
+        fecha_fundacion : {type : String, required : false},
+        referencia_historia : {type : String, required : false},
+        adjuntar_documentos : {type : String, required : false},
+        fax : {type : String, required : false},
+        sitio_web : {type : String, required : false},
+        cuentas_redesso : {type : String, required : false},
+        contacto_nombre : {type : String, required : false},
+        departamento : {type : String, required : false}, 
+        telefono_contacto : {type : String, required : false},
         extension_contacto : {type : String, required : false},
-        correo_electronico_contacto : {type : String, required : true},
+        correo_electronico_contacto : {type : String, required : false},
         foto_contacto : {type : String, required : false},
+
+        //Todos los usuarios
+        tipo_usuario : {type : String, required : false},
+        estado : {type : Boolean, required : false},
     }
 );
 
-module.exports = mongoose.model('centro_educativo', schema_centroe);
+module.exports = mongoose.model('usuarios', schema_usuarios);
--- proyecto/prueba/api/componentes/usuarios/usuarios.route.js
+++ proyecto/prueba/api/componentes/usuarios/usuarios.route.js
@@ -3,18 +3,37 @@ const express = require('express');
 const router = express.Router();
 const api_usuarios = require('./usuarios.api');
 
-router.route('/centroe_nuevo')
+//end point registro ce
+router.route('/registrar_ce')
 .post(
     function(req, res){
-        api_usuarios.registrar(req, res);
+        api_usuarios.registrar_ce(req, res);
     }
 );
+//end point lista ce
 
 router.route('/listar_centroe')
-        .get(
-            function(req, res){
-                api_usuarios.listar_centroe(req, res);
-            }
-        )
+    .get(
+        function(req, res){
+            api_usuarios.listar_ce(req, res);
+        }
+    )
+//end point lista todos
+
+router.route('/listar_todos')
+    .get(
+        function(req, res){
+            api_usuarios.listar_todos(req, res);
+    }
+)
+
+//end point lista ce pendientes
+
+router.route('/listar_ce_pendientes')
+    .get(
+        function(req, res){
+            api_usuarios.listar_solicitud_pendiente(req, res);
+    }
+)
 
 module.exports = router;
