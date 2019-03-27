ce05a97b8d395fcddac81f7989ea1ecf61f5274c
Miguel Gutierrez <mgutierrezb@ucenfotec.ac.cr>
2019-03-26 23:10:38 -0600

    arreglos varios

--- prueba/public/js/etiquetas/controlador_listar_etiquetas.js
+++ prueba/public/js/etiquetas/controlador_listar_etiquetas.js
@@ -7,9 +7,10 @@ let mostrar_datos = () =>{
     for(let i =0; i < etiquetas.length; i++){
         let fila = tabla.insertRow(); //Linea crea el tr de la table
         fila.insertCell().innerHTML = etiquetas[i]['nombre'];
-        fila.insertCell().innerHTML ='<i class="fas fa-pen-square"></i>';
-        fila.insertCell().innerHTML ='<i class="fas fa-trash-alt"></i>';
-        
+        fila.insertCell().innerHTML ='<button type="button" id="btn_asociar">Asociar</button>';
+        fila.insertCell().innerHTML ='<button type="button" id="btn_modificar">Modificar</button>';
+        fila.insertCell().innerHTML ='<button type="button" id="btn_eliminar">Eliminar</button>';
+
     };
 
 };
