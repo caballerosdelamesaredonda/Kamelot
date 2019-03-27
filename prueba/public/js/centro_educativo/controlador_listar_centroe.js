d3c6e26c563c3b938b1354545d13ebf7c07df747
FSANDOVALC <47130944+FSANDOVALC@users.noreply.github.com>
2019-03-27 12:36:40 -0600

    Update obtain id from tr

--- prueba/public/js/centro_educativo/controlador_listar_centroe.js
+++ prueba/public/js/centro_educativo/controlador_listar_centroe.js
@@ -1,20 +1,28 @@
 'use strict';
 
 const tabla = document.querySelector('#tbl_centroe tbody');
+let tablerowvalue = '';
 
-let mostrar_datos = () =>{
+
+
+let mostrar_datos = () => {
     let usuarios = listar_centroe();
-    for(let i =0; i < usuarios.length; i++){
+    for (let i = 0; i < usuarios.length; i++) {
         let fila = tabla.insertRow(); //Linea crea el tr de la table
-        fila.setAttribute('id',usuarios[i]['_id']);
+        fila.setAttribute('id', usuarios[i]['_id']);
         fila.insertCell().innerHTML = usuarios[i]['nombre'];
         fila.insertCell().innerHTML = usuarios[i]['cedula_juridica'];
         fila.insertCell().innerHTML = usuarios[i]['correo_electronico'];
         fila.insertCell().innerHTML = usuarios[i]['telefono'];
-        
     };
-
 };
 
-
 mostrar_datos();
+
+$("#tbl_centroe tbody").on("click", "tr", function () {
+    var Name = $(this).find('td:first').text();
+    var ID = $(this).closest('tr').attr('id');
+    alert(Name);
+    alert(ID);
+});
+
