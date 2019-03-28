4ce893f827b8ec9804b20c8128b1b9e06d58c083
aramirezncenfo <aramirezn@ucenfotec.ac.cr>
2019-03-27 19:52:13 -0600

    listar usuarios

--- proyecto/prueba/public/js/controlador_listar_usuarios.js
+++ proyecto/prueba/public/js/controlador_listar_usuarios.js
@@ -1,26 +1,32 @@
 'use strict';
 
-const inputFiltrarUsuario = document.querySelector('#textBusquedaUsuario');
+const input_busqueda = document.querySelector('#textBusqueda');
 
 const tabla = document.querySelector('#tbl_actividad tbody');
 
+//recibe tipo de filtro, ejm: "tipo", "nombre",
+let filtrar_por = (filtro, valor) =>{
+    return usuarios[`${filtro}`] == valor;
+};
 
 let mostrar_datos = () =>{
 
     let usuarios = obtener_usuarios;
 
-    let filtrar_por = (filtro, valor) =>{
-        return usuarios[`${filtro}`] == valor;
-    };
+    let usuarios_filtrados = usuarios.filter(filtrar_por('nombre', input_busqueda.value));
 
-    let usuarios_filtrados = usuarios.filter(filtrar_por())
+    let tabla = document.querySelector('#tabla_usuarios tbody');
+    tabla.innerHTML = '';
 
-
-
-    let actividad = listar_actividad();
-    for(let i = 0; i < actividad.length; i++){
+    for(let i = 0; i < usuarios_filtrados.length; i++){
 
         let fila = tabla.insertRow();// Crea el tr de la tabla
+
+        let celda_nombre = fila.insertCell();
+        let celda_apellido = fila.insertCell();
+        let celda_correo = fila.insertCell();
+        let celda_tipo = fila.insertCell();
+        let estado = fila.insertCell();
 
         fila.insertCell().innerHTML = actividad[i]['actividad'];
     };
