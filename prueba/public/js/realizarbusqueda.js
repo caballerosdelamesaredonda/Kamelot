9ed64fdda3f3472cc457cb19777ed4c7cefd2d44
Miguel Gutierrez <mgutierrezb@ucenfotec.ac.cr>
2019-03-27 20:13:29 -0600

    Push

--- prueba/public/js/realizarbusqueda.js
+++ prueba/public/js/realizarbusqueda.js
@@ -13,16 +13,25 @@ let mostrar_datos_filtrados = () => {
     for (let i = 0; i < usuarios.length; i++) {
         if (
         (usuarios[i]['nombre'].toLowerCase().includes(filtro.toLowerCase()))
-        || (usuarios[i]['cedula_juridica'].toLowerCase().includes(filtro.toLowerCase()))
         || (usuarios[i]['correo_electronico'].toLowerCase().includes(filtro.toLowerCase()))
         || (usuarios[i]['telefono'].toLowerCase().includes(filtro.toLowerCase()))
+        || (usuarios[i]['tipo_centro'].toLowerCase().includes(filtro.toLowerCase()))
+        || (usuarios[i]['nivel_centro'].toLowerCase().includes(filtro.toLowerCase()))
+        || (usuarios[i]['provincia'].toLowerCase().includes(filtro.toLowerCase()))
+        || (usuarios[i]['canton'].toLowerCase().includes(filtro.toLowerCase()))
+
         ) {
             let fila = tabla.insertRow(); // Crea tr de la tabla
             fila.setAttribute('id',usuarios[i]['_id']);
+            fila.insertCell().innerHTML = `<img src="${usuarios[i]['foto']}" height="60px" width="60px"> `
             fila.insertCell().innerHTML = usuarios[i]['nombre'];
-            fila.insertCell().innerHTML = usuarios[i]['cedula_juridica'];
             fila.insertCell().innerHTML = usuarios[i]['correo_electronico'];
             fila.insertCell().innerHTML = usuarios[i]['telefono'];
+            fila.insertCell().innerHTML = usuarios[i]['tipo_centro'];
+            fila.insertCell().innerHTML = usuarios[i]['nivel_centro'];
+            fila.insertCell().innerHTML = usuarios[i]['provincia'];
+            fila.insertCell().innerHTML = usuarios[i]['canton'];
+
         }
     }
 
