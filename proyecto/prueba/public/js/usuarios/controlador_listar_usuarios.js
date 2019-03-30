65b2ff24ec31c6610f6204c3ef67167f412ce9dc
Alexander Ramirez N <aramirezn@ucenfotec.ac.cr>
2019-03-30 14:59:10 -0600

    lu

--- proyecto/prueba/public/js/usuarios/controlador_listar_usuarios.js
+++ proyecto/prueba/public/js/usuarios/controlador_listar_usuarios.js
@@ -7,29 +7,44 @@ let tabla = document.querySelector('#tbl_usuarios tbody');
 
 let mostrar_datos = () =>{
 
+
     let busqueda = '';
     if (input_busqueda.value !== null){
         busqueda = input_busqueda.value;
     }
     let resultado =[];
 
-    resultado = usuarios.filter(usuario => (busqueda.length > 0 ? usuario.nombre.toLowerCase().includes(busqueda.toLowerCase()) : usuario));
+    resultado = usuarios.filter(usuario => (
+        busqueda.length > 0 ? usuario.nombre.toLowerCase().trim().includes(
+            busqueda.toLowerCase().trim()
+            ) : usuario
+        )
+    );
+
+    console.log('Se encontró un: ' + typeof resultado + ':');
+    console.log(resultado);
 
 
 
+    tabla.innerHTML = '';
 
     for(let i = 0; i < resultado.length; i++){
 
         let fila = tabla.insertRow();// Crea el tr de la tabla
 
         let celda_nombre = fila.insertCell();
-        let celda_apellido = fila.insertCell();
         let celda_correo = fila.insertCell();
         let celda_tipo = fila.insertCell();
         let estado = fila.insertCell();
 
         celda_nombre.innerHTML = resultado[i]['nombre'];
-        celda_apellido.innerHTML = resultado[i]['papellido'];
+        
+        if (resultado[i]['tipo_usuario'] === 'PF') {
+            celda_nombre.innerHTML = resultado[i]['nombre'] +' '+resultado[i]['papellido'];
+        }else {
+            celda_nombre.innerHTML = resultado[i]['nombre'];
+        }
+
         celda_correo.innerHTML = resultado[i]['correo_electronico'];
         celda_tipo.innerHTML = resultado[i]['tipo_usuario'];
         estado.innerHTML = resultado[i]['estado'];
