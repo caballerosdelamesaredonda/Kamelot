1074ab9c8bddbb93ad4f8d15b88030166f1e97f0
Alexander Ramirez N <aramirezn@ucenfotec.ac.cr>
2019-03-29 18:32:35 -0600

    lu

--- proyecto/prueba/public/js/controlador_listar_usuarios.js
+++ proyecto/prueba/public/js/controlador_listar_usuarios.js
@@ -2,26 +2,23 @@
 
 const input_busqueda = document.querySelector('#txt_buscar');
 
-let usuarios = listar_usuarios;
-let tabla = document.querySelector('#tabla_usuarios tbody');
+let usuarios = listar_usuarios();
+let tabla = document.querySelector('#tbl_usuarios tbody');
 
 let mostrar_datos = () =>{
 
-
-
-    /*
     let busqueda = '';
     if (input_busqueda.value !== null){
         busqueda = input_busqueda.value;
     }
     let resultado =[];
 
-    resultado = usuarios.filter(usuario => {busqueda.length > 0 ? usuario.nombre.toLowerCase().includes(busqueda.toLowerCase()) : usuario});
-
-    */
+    resultado = usuarios.filter(usuario => (busqueda.length > 0 ? usuario.nombre.toLowerCase().includes(busqueda.toLowerCase()) : usuario));
 
 
-    for(let i = 0; i < usuarios.length; i++){
+
+
+    for(let i = 0; i < resultado.length; i++){
 
         let fila = tabla.insertRow();// Crea el tr de la tabla
 
@@ -31,14 +28,15 @@ let mostrar_datos = () =>{
         let celda_tipo = fila.insertCell();
         let estado = fila.insertCell();
 
-        celda_nombre.innerHTML = usuarios[i]['nombre'];
-        celda_apellido.innerHTML = usuarios[i]['papellido'];
-        celda_correo.innerHTML = usuarios[i]['correo_electronico'];
-        celda_tipo.innerHTML = usuarios[i]['tipo_usuario'];
-        estado.innerHTML = usuarios[i]['estado'];
+        celda_nombre.innerHTML = resultado[i]['nombre'];
+        celda_apellido.innerHTML = resultado[i]['papellido'];
+        celda_correo.innerHTML = resultado[i]['correo_electronico'];
+        celda_tipo.innerHTML = resultado[i]['tipo_usuario'];
+        estado.innerHTML = resultado[i]['estado'];
 
     }
 
 };
 
 mostrar_datos();
+input_busqueda.addEventListener('keyup', mostrar_datos);
