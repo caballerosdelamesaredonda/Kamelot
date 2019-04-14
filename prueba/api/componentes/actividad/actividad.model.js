61a3b6394452839971bf281b48bd7cf321dc7f34
caballerosdelamesaredonda <loscaballerosdelamesaredonnda@gmail.com>
2019-04-14 17:49:24 -0600

    css
    css

--- prueba/api/componentes/actividad/actividad.model.js
+++ prueba/api/componentes/actividad/actividad.model.js
@@ -6,7 +6,8 @@ let schema_actividad = new mongoose.Schema(
         userid : {type : String, required : true},
         titulo : {type : String, required : true},
         actividad : {type : String, required : true},
-        foto :{type : String, required : true}
+        foto :{type : String, required : true},
+        estado : {type : String, required : true}
     }
 );
 
