d7ec418396943edc9238c1a9aee0dd9c8976423d
caballerosdelamesaredonda <loscaballerosdelamesaredonnda@gmail.com>
2019-04-19 21:05:23 -0600

    css
    css

---
+++ prueba/public/js/pregunta/controlador_listar_pregunta.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const tabla = document.querySelector('#tbl_preguntas tbody');
+
+let mostrar_datos = () =>{
+    let pregunta = listar_pregunta();
+    for(let i = 0; i < pregunta.length; i++){
+
+        let fila = tabla.insertRow();// Crea el tr de la tabla
+
+        fila.insertCell().innerHTML = pregunta[i]['pregunta'];
+        fila.insertCell().innerHTML = respuesta[i]['respuesta'];
+       
+    }; 
+
+};
+
+
+mostrar_datos();
