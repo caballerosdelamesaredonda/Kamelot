25865f38e02da80ee3d52e5869337f38605fab6a
caballerosdelamesaredonda <47197823+caballerosdelamesaredonda@users.noreply.github.com>
2019-04-25 12:17:08 -0600

    RE-Upload de cambiar contraseña

--- prueba/public/js/sesion/controlador_iniciar_sesion.js
+++ prueba/public/js/sesion/controlador_iniciar_sesion.js
@@ -3,7 +3,7 @@
 const input_correo = document.querySelector('#txt_correo');
 const input_contrasenna = document.querySelector('#txt_contrasenna');
 const boton_iniciar_sesion = document.querySelector('#btn_ingresar');
-
+const link_cambiar_clave = document.querySelector('#cambiar_clave');
 
 let validar = () =>{
 	let error = false;
@@ -39,7 +39,14 @@ let obtenerDatos = () =>{
 				let validacion_credenciales = validar_credenciales(correo, contrasenna);
 
 				if(validacion_credenciales.success){
-						iniciar_sesion(validacion_credenciales.usuario);
+						let temporal = validacion_credenciales.usuario.temporal;
+						if (temporal === 'si'){
+							localStorage.usuario_en_sesion = validacion_credenciales.usuario['_id'];
+							window.location.href=`cambiar_clave.html?id=${validacion_credenciales.usuario['_id']}`;
+						}else{
+							iniciar_sesion(validacion_credenciales.usuario);
+						}
+
 				}
 
 		}else{
@@ -71,4 +78,26 @@ let iniciar_sesion =(pUsuario)=> {
 
 };
 
+link_cambiar_clave.addEventListener('click', async function () {
+	let {value: email} = await Swal.fire({
+		title: 'Ingresa tu correo electrónico',
+		input: 'email',
+		inputPlaceholder: 'correo electrónico'
+	});
+	if (email) {
+		let res_validacion = validar_correo(email);
+		if (res_validacion.success){
+			Swal.fire('Tu código temporal se ha enviado a tu correo electrónico. ')
+		}else{
+			console.log(res_validacion.msg)
+			Swal.fire({
+				type: 'error',
+				text:'Correo electrónico invalido, intenta de nuevo'
+			});
+		}
+
+	}
+});
 boton_iniciar_sesion.addEventListener('click' , obtenerDatos);
+
+
