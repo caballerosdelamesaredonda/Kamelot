9590b3ef70293e164c28dd27e578b58ea59bbb2b
Miguel Gutierrez <mgutierrezb@ucenfotec.ac.cr>
2019-04-26 16:02:56 -0600

    Update controlador_listar_etiquetasce.js

--- prueba/public/js/etiquetas/controlador_listar_etiquetasce.js
+++ prueba/public/js/etiquetas/controlador_listar_etiquetasce.js
@@ -13,11 +13,32 @@ let mostrar_datos = () =>{
     for(let i =0; i < etiquetas.length; i++){
         let fila = tabla.insertRow(); //Linea crea el tr de la table
         fila.insertCell().innerHTML = etiquetas[i]['nombre'];
-        fila.insertCell().innerHTML ='<button type="button" id="btn_asociar">Asociar</button>';
-        fila.insertCell().innerHTML ='<button type="button" id="btn_modificar">Remover</button>';
+        fila.insertCell().innerHTML ='<button class="btn_editar_noticia" style="border: none; background-color: inherit; margin-left: 3px"><img src="/public/images/edit.png" style="width: 30px; height:30px;"></button>';
+        fila.insertCell().innerHTML ='<button class="btn_borrar_noticia" style="border: none; background-color: inherit; margin-left: 3px"><img src="/public/images/garbage.png" style="width: 30px; height:30px;"></button>';
     };
 
 };
 
 
+
 mostrar_datos();
+
+$('.btn_editar_noticia').click(function () {
+    swal.fire({
+        type: 'error',
+        title: 'Las etiquetas se encuentran bajo mantenimiento',
+        text: 'Por favor intente luego'
+    }).then(function () {
+        window.location = "/public/perfil_centroedu.html";
+    });
+});
+
+$('.btn_borrar_noticia').click(function () {
+    swal.fire({
+        type: 'error',
+        title: 'Las etiquetas se encuentran bajo mantenimiento',
+        text: 'Por favor intente luego'
+    }).then(function () {
+        window.location = "/public/perfil_centroedu.html";
+    });
+});
